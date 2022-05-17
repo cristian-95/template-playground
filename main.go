1200f331cded6cc6dc2ccb3bf2ccc61dffb42ac3
cristian-95 <nem.cristianrc@gmail.com>
2022-05-17 20:20:15 -0300

    adicionado template, porem conteudo ainda não é renderizado na pagina html

--- main.go
+++ main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/", nextWeek)
-
+	http.HandleFunc("/", myTemplateHandler)
+	fmt.Println(nextWeek().String())
 	log.Print("serving on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func nextWeek(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./public/index.html")
+func myTemplateHandler(w http.ResponseWriter, r *http.Request) {
+	releaseDate := nextWeek()
+	Date := releaseDate.String()
+
+	tmpl, _ := template.ParseFiles("./public/index.html")
+	tmpl.Execute(w, Date)
+}
+
+func nextWeek() time.Time {
+	rd := time.Now()
+	return rd.Add(7 * 24 * time.Hour)
 
 }
